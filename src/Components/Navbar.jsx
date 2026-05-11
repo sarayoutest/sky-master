5c2ddcedaee60654f6cc5c9df7461098cd9788b0
sara youssef <154534692+sarayoutest@users.noreply.github.com>
2026-05-11 16:32:40 +0300

    Update Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -28,7 +28,7 @@ import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import MenuIcon from "@mui/icons-material/Menu";
 import { Link, useNavigate } from "react-router-dom";
 import routes from "../routes";
-
+import logo from "../assets/images/Logo.svg";
 /* ===== Shared Styles ===== */
 const navBtn = {
   color: "#6b5a86",
@@ -101,7 +101,7 @@ const [mobileWorkOpen, setMobileWorkOpen] = useState(false);
   <Box>
     <Link to="/">
       <img
-        src="./src/assets/images/logo.svg"
+       src={logo}
         alt="logo"
         style={{ height: isMobile ? 38 : 50 }}
       />
