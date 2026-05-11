f8c7ac93682ef176a3f569c8700d60fc90c7bcac
sara youssef <154534692+sarayoutest@users.noreply.github.com>
2026-05-11 14:53:23 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/sky-master">
+    <BrowserRouter>
       <UserProvider> {/* 2. تغليف التطبيق بـ UserProvider */}
         <CartProvider>
           <App />
